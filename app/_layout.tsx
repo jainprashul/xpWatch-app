e7d4f7c2410dc9a5ddf2353bbd8c42c0408bc746
Prashul Jain <jainprashul@gmail.com>
2023-08-20 14:29:56 +0530

    Added Eas Update Config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,8 +9,27 @@ import { theme } from '../style/theme';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useAppStart } from '../components/useAppStart';
 import { StatusBar } from 'expo-status-bar';
+import * as Updates from 'expo-updates';
+import { useEffect } from 'react';
 
 export default function Layout() {
+
+  Updates.useUpdateEvents(async (event) => {
+    if (event.type === Updates.UpdateEventType.UPDATE_AVAILABLE) {
+      await Updates.fetchUpdateAsync()
+      alert('Update downloaded, will install now');
+      await Updates.reloadAsync();
+    } else if (event.type === Updates.UpdateEventType.NO_UPDATE_AVAILABLE) {
+      console.log('No update available');
+    } else if (event.type === Updates.UpdateEventType.ERROR) {
+      // handle error
+      console.log('Error while checking for updates', event.message);
+      alert('Error while checking for updates');
+    } 
+
+
+  });
+
   return (
     <>
       <Provider store={store}>
